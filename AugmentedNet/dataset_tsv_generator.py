15d20e6e7344e808ea3fd812dbdd0c5b32d441fd
Johannes Hentschel <johannes.hentschel@epfl.ch>
2025-03-16 21:33:41 +0100

    factors out store_labeled_pitch_array_and_label_tsv()

--- AugmentedNet/dataset_tsv_generator.py
+++ AugmentedNet/dataset_tsv_generator.py
@@ -60,6 +60,38 @@ def generateDataset(synthesize=False, texturize=False, tsvDir="dataset", eventBa
             df.to_csv(os.path.join(datasetDir, DATASETSUMMARYFILE), sep="\t")
     return df
 
+def store_labeled_pitch_array_and_label_tsv(
+        nickname: str,
+        score_path: str,
+        annotation_path: str,
+        datasetDir: str,
+        split: str,
+        assembled_dir: str,
+        include_metadata: bool = True
+):
+    extended_adf, sdf, jointdf, metadata = parseAnnotationAndScoreEvents(annotation_path, score_path)
+    for df, suffix in [(jointdf, "joint")]:  # , (sdf, "slices")]:
+        outpath = os.path.join(datasetDir, split, f"{nickname}_{suffix}.tsv")
+        df.to_csv(outpath, sep="\t", index=False)
+    outpath = os.path.join(assembled_dir, "labels", f"{nickname}.tsv")
+    extended_adf.to_csv(outpath, sep="\t", index=False)
+    # copy and rename original score
+    _, score_ext = os.path.splitext(score_path)
+    new_score_path = os.path.join(datasetDir, split, f"{nickname}{score_ext}")
+    shutil.copy(score_path, new_score_path)
+    collection = nickname.split("-")[0]
+    stats = dict(
+        file=nickname,
+        annotation=annotation_path,
+        score=score_path,
+        collection=collection,
+        split=split
+    )
+    if include_metadata:
+        stats.update(metadata)
+    return stats
+
+
 def generateEventsDataset(tsvDir="events", assembled_dir="assembled", include_metadata=True):
     statsrecords = []
     datasetDir = tsvDir
@@ -69,26 +101,9 @@ def generateEventsDataset(tsvDir="events", assembled_dir="assembled", include_me
         for nickname in files:
             print(nickname)
             annotation, score = ANNOTATIONSCOREDUPLES[nickname]
-            extended_adf, sdf, jointdf, metadata = parseAnnotationAndScoreEvents(annotation, score)
-            for df, suffix in [(jointdf, "joint")]: #, (sdf, "slices")]:
-                outpath = os.path.join(datasetDir, split, f"{nickname}_{suffix}.tsv")
-                df.to_csv(outpath, sep="\t")
-            outpath = os.path.join(assembled_dir, "labels", f"{nickname}.tsv")
-            extended_adf.to_csv(outpath, sep="\t", index=False)
-            # copy and rename original score
-            _, score_ext = os.path.splitext(score)
-            new_score_path = os.path.join(datasetDir, split, f"{nickname}{score_ext}")
-            shutil.copy(score, new_score_path)
-            collection = nickname.split("-")[0]
-            stats = dict(
-                file = nickname,
-                annotation = annotation,
-                score = score,
-                collection = collection,
-                split = split
-            )
-            if include_metadata:
-                stats.update(metadata)
+            stats = store_labeled_pitch_array_and_label_tsv(
+                nickname, score, annotation, datasetDir, split, assembled_dir, include_metadata
+                )
             statsrecords.append(stats)
             # misalignment = jointdf.measureMisalignment.mean().round(2)
             # statsdict["misalignmentMean"].append(misalignment)
@@ -99,6 +114,7 @@ def generateEventsDataset(tsvDir="events", assembled_dir="assembled", include_me
             jointdf = pd.DataFrame.from_records(statsrecords)
             jointdf.to_csv(os.path.join(datasetDir, DATASETSUMMARYFILE), sep="\t")
     return jointdf
+
 
 if __name__ == "__main__":
     parser = cli.tsv()
